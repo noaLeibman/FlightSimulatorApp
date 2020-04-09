9865bea94b48a0b8c94ad97fc3c8e6d5b0b64227
noa Leibman <noale27@gmail.com>
2020-04-09 20:48:57 +0300

    forgot main VM

---
+++ FlightSimulatorApp/view_models/VM_Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightSimulatorApp.Model;
+using System.ComponentModel;
+
+
+namespace FlightSimulatorApp.view_models
+{
+    class VM_Main : INotifyPropertyChanged
+    {
+        public VM_Map map;
+        public VM_ControlPanel cp;
+        public VM_JoystickControl steers;
+        private ISimulatorModel model;
+        public VM_Main(ISimulatorModel model)
+        {
+            this.model = model;
+            this.map = new VM_Map();
+            this.map.SetModel(model);
+            this.cp = new VM_ControlPanel();
+            this.steers = new VM_JoystickControl();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
