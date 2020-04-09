1bb3b370648ca53b45114772f61f4729a21ec16d
noa Leibman <noale27@gmail.com>
2020-04-09 12:10:46 +0300

    first commit - map+control panel

---
+++ FlightSimulatorApp/view_models/VM_ControlPanel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorApp.view_models
+{
+    class VM_ControlPanel
+    {
+        public string VM_heading { get { return "8"; } }
+        public string VM_verticalSpeed { get { return "309"; } }
+        public string VM_groundSpeed { get { return "88"; } }
+        public string VM_airspeed { get { return "44444"; } }
+        public string VM_gpsAltitude { get { return "1234"; } }
+        public string VM_roll { get { return "55"; } }
+        public string VM_pitch { get { return "32"; } }
+        public string VM_altimeterAltitude { get { return "35"; } }
+        public VM_ControlPanel()
+        {
+        }
+
+    }
+}
